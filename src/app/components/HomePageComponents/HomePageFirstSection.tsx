d7164be8ecdd9f8aa1083b6ee6efcc1a9fbed4aa
xkolumber <lubosk158@gmail.com>
2024-07-08 16:10:10 +0200

    different approach for placeholder

--- src/app/components/HomePageComponents/HomePageFirstSection.tsx
+++ src/app/components/HomePageComponents/HomePageFirstSection.tsx
@@ -3,7 +3,9 @@ import HomePageLoop from "./HomePageLoop";
 import getBase64 from "@/app/lib/functions";
 
 const HomePageFirstSection = async () => {
-  const url = await getBase64(`${process.env.URL}/loop/main1.jpg`);
+  // const url = await getBase64(`${process.env.URL}/loop/main1.jpg`);
+  const url =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAIAAAA7ljmRAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAMElEQVR4nGPoyAnauXnji/+fbVNdGZqyAqe1FtlEajHIMzD42mhFBFjwaAgw8DEAAEdSDLK7z3GTAAAAAElFTkSuQmCC";
 
   return <HomePageLoop blurUrl={url} />;
 };
