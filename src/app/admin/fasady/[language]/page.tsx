a35b9010bcca918b8b7ca6160e568778d0728514
xkolumber <lubosk158@gmail.com>
2024-07-24 14:57:26 +0200

    admin facades page

---
+++ src/app/admin/fasady/[language]/page.tsx
@@ -0,0 +1,54 @@
+import AdminFasady from "@/app/components/AdminComponents/AdminFasady";
+import AdminFinalNotAuthorized from "@/app/components/AdminComponents/AdminFinalNotAuthorized";
+import AdminNotAuthorized from "@/app/components/AdminComponents/AdminNotAuthorized";
+import AdminOstatne from "@/app/components/AdminComponents/AdminOstatne";
+import AdminPageSkeleton from "@/app/components/AdminComponents/AdminPageSkeleton";
+import { GetLanguages, getToken } from "@/app/lib/actions";
+import { GetAdminFasady, GetAdminOstatne } from "@/app/lib/functionsServer";
+import jwt from "jsonwebtoken";
+import { Suspense } from "react";
+
+async function Validate(language: string) {
+  const authToken = await getToken();
+
+  if (!authToken) {
+    return <AdminNotAuthorized />;
+  }
+
+  const decodedToken: any = jwt.decode(authToken!);
+  if (!decodedToken || typeof decodedToken === "string") {
+    return <AdminNotAuthorized />;
+  }
+  const browser_uid = decodedToken.user_id;
+
+  if (browser_uid === process.env.ADMIN_UID) {
+    const languages = await GetLanguages();
+
+    const data = await GetAdminFasady(language);
+    if (data) {
+      return (
+        <AdminFasady language={language} data={data} languages={languages} />
+      );
+    }
+
+    return (
+      <AdminFasady language={language} data={undefined} languages={languages} />
+    );
+  } else {
+    return <AdminFinalNotAuthorized />;
+  }
+}
+
+type Props = {
+  params: { language: string };
+};
+
+const Page = ({ params }: Props) => {
+  return (
+    <Suspense fallback={<AdminPageSkeleton />}>
+      {Validate(params.language)}
+    </Suspense>
+  );
+};
+
+export default Page;
